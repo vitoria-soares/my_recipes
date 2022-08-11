c6eb8491231821bd82d04b39e97cd6791bda8a0d
vitoria-soares <vitoria.helen.c.soares@gmail.com>
2022-08-11 00:20:35 -0300

    #initial_settings

---
+++ dependencies_module/lib/dependencies_module.dart
@@ -0,0 +1,8 @@
+library dependencies_module;
+
+export 'package:bloc/bloc.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:dio/dio.dart';
+export 'package:mocktail/mocktail.dart';
+export 'package:bloc_test/bloc_test.dart';
---
+++ my_recipes_application/android/app/src/main/kotlin/com/example/my_recipes_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_recipes_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
