c0aea4d0cdaf1b04533e0629b616486e87360aaf
vitoria-soares <vitoria.helen.c.soares@gmail.com>
2022-08-11 17:56:02 -0300

    #external - done

---
+++ my_recipes_application/test/utils/recipe_json.dart
@@ -0,0 +1,11 @@
+String recipeJsonMock = '''
+{
+"meals": [
+        {
+            "strMeal": "Baked salmon with fennel & tomatoes",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/1548772327.jpg",
+            "idMeal": "52959"
+        }
+      ]
+}
+''';
