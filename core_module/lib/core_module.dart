b8193d6647dfe862a6df4af7569d3efaa0496ceb
vitoria-soares <vitoria.helen.c.soares@gmail.com>
2022-08-11 15:23:28 -0300

    #datasource - done

--- core_module/lib/core_module.dart
+++ core_module/lib/core_module.dart
@@ -1,4 +1,6 @@
 library core_module;
 
+export 'package:core_module/src/services/dio/dio_service_interface.dart';
+export 'package:core_module/src/services/dio/service_information.dart';
 export 'package:core_module/src/errors/application_error.dart';
 export 'package:core_module/src/services/dio/dio_service_implementation.dart';
